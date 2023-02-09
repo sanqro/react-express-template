909683489afef97b7d1282104084931fb7fc69b7
sanqro <contact@sanqro.xyz>
2023-02-09 16:23:08 +0100

    add express project

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+// root endpoint
+app.get("/", (req, res) => res.send("This is the root endpoint! "));
+
+app.listen(3000, () => {
+  console.log("🏃 Running on http://localhost:3000");
+})
+
+module.exports = app;
