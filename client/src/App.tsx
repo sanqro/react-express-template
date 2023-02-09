b4d67d9eae5375eb77b929f3b0aeb33f6ecdb256
sanqro <contact@sanqro.xyz>
2023-02-09 16:49:32 +0100

    set up eslint and prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <h1 className="text-8xl font-bold">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-8xl font-bold">Hello world!</h1>;
 }
 
 export default App;
